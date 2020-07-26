a19fdc435382165519c46812692799c89bd70c29
Aaron Corson <chaopivoo@live.com>
2020-07-26 12:53:58 -0700

    created randomize Array function

---
+++ index.py
@@ -0,0 +1,20 @@
+#========================================#
+# Imported Libaries
+#========================================#
+from random import random
+from datetime import datetime
+
+#========================================#
+# Randomize Array
+#========================================#
+
+def createRandomArray():
+    arr = [];
+    for i in range(10):
+        arr.append(random()*100);
+    return arr;
+
+
+#========================================#
+# Initialization
+#========================================#
